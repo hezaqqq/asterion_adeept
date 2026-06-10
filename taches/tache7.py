d037328b1c17cb2dac48fddf84f764f9c6fae657
Félix Lang <felixlang1004@gmail.com>
2026-06-10 11:56:34 +0200

    ezofoko

--- taches/tache7.py
+++ taches/tache7.py
@@ -1,6 +1,32 @@
-import tache1.py as t1
-import tache2.py as t2
-import tache3.py as t3
-import tache4.py as t4
-import tache5.py as t5
-import tache6.py as t6
+import tache1 as t1
+import tache2 as t2
+import tache3 as t3
+import tache4 as t4
+import tache5 as t5
+import tache6 as t6
+
+robot_led_controller = t1.RobotLEDController()
+led_controller = t2.LEDController(led_count=14)
+servo_controller = t3.ServoController()
+servo_sequence = t3.ServoSequence(servo_controller)
+servo_manual = t3.ServoManual(servo_controller)
+motor_controller = t4.MotorController()
+distance_sensor = t5.Distance()
+line_follower = t6.LineFollower()
+
+action = input("Entrez les numéros de tâche à exécuter séparés d'un espace : ").strip().split()
+for i in range(len(action)):
+    if action[i] == '1':
+        t1.__main__()
+    elif action[i] == '2':
+        t2.__main__()
+    elif action[i] == '3':
+        t3.__main__()
+    elif action[i] == '4':
+        t4.__main__()
+    elif action[i] == '5':
+        t5.__main__()
+    elif action[i] == '6':
+        t6.__main__()
+
+        
