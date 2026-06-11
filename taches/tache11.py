5b42a7a0edc850a143e4929644687f9f5c0f6f4a
Félix Lang <felixlang1004@gmail.com>
2026-06-11 12:24:37 +0200

    com

--- taches/tache11.py
+++ taches/tache11.py
@@ -58,7 +58,7 @@ if __name__ == "__main__":
                 if robot.en_marche:
                     robot.arreter()
                     controller.set_angle(0, current_angle)
-                    robot.mc.drive_ramp(-t9.RobotController.VITESSE_MARCHE, ramp_time=elapsed-0.02)
+                    robot.mc.drive_ramp(-t9.RobotController.VITESSE_MARCHE, ramp_time=elapsed)
                 ligne_perdue_ts = None  # reset pour retenter
 
                 robot.demarrer()
