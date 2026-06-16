2161e64d0fcace6e426b06afb77432ecf61a0936
Félix Lang <felixlang1004@gmail.com>
2026-06-16 11:44:57 +0200

    com

--- taches/detection_et_esquive_obstacles.py
+++ taches/detection_et_esquive_obstacles.py
@@ -8,11 +8,29 @@ ANGLE_CENTER_ROUE  = 100
 ANGLE_MIN_ROUE     = 60
 ANGLE_MAX_ROUE     = 140
 
+ANGLE_CENTER_TETE_GD  = 108
+ANGLE_MIN_TETE_GD     = 68
+ANGLE_MAX_TETE_GD     = 148
+
 if __name__ == "__main__":
     try:
+        angle_tete_gd = ANGLE_CENTER_TETE_GD
         tete = t3.ServoController()
         tete.set_angle(2, 85)
-        tete.set_angle(1, 105)
+        tete.set_angle(1, angle_tete_gd)
+        gauche = True
+
+        while True:
+            if angle_tete_gd < ANGLE_MAX_TETE_GD and gauche:
+                angle_tete_gd += 1
+            elif angle_tete_gd > ANGLE_MIN_TETE_GD and not gauche:
+                angle_tete_gd -= 1
+            elif angle_tete_gd >= ANGLE_MAX_TETE_GD:
+                gauche = False
+            elif angle_tete_gd <= ANGLE_MIN_TETE_GD:
+                gauche = True
+
+            time.sleep(0.05)
     
     except KeyboardInterrupt:
         pass  
