ef6662441afd43dd9551740613a81a3f0eadd8e9
Félix Lang <felixlang1004@gmail.com>
2026-06-17 10:39:06 +0200

    com

--- taches/detection_et_esquive_obstacles.py
+++ taches/detection_et_esquive_obstacles.py
@@ -17,7 +17,7 @@ if __name__ == "__main__":
         controller = t3.ServoController()
         angle_tete_gd = ANGLE_CENTER_TETE_GD
         controller.set_angle(1, angle_tete_gd)
-        controller.set_angle(2, 85)
+        controller.set_angle(2, 75)
         gauche = True
 
         sensor = t5.Distance()
